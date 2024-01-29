893ebe817dac957f9bef8341e81e23535f03c975
UmairAlam99 <umairalam121212@gmail.com>
2024-01-29 04:44:41 -0800

    secondt commit

--- Question_04.dart
+++ Question_04.dart
@@ -2,14 +2,14 @@ void main() {
 // Q.4: Create a list of numbers & write a program to get the smallest & greatest
 // number from a list.,
   List numbers = [45, 85, 76, 99, 12];
-  int smallestValue = numbers[0]; //initilize for first element
-  int greatesttValue = numbers[0]; //initilize for first element
+  int smallestValue = numbers[0];
+  int greatesttValue = numbers[0];
   //for smallest value
   for (var number in numbers) {
     if (number < smallestValue) {
       smallestValue = number;
     }
-  }
+  } //for smallest value
   for (var number2 in numbers) {
     if (number2 > greatesttValue) {
       greatesttValue = number2;
@@ -19,9 +19,3 @@ void main() {
   print("the smallest value is $smallestValue");
   print("the greatest value is $greatesttValue");
 }
-git init
-git add README.md
-git commit -m "first commit"
-git branch -M main
-git remote add origin https://github.com/UmairAlam99/Assignment-3-1-to-10-.git
-git push -u origin main
